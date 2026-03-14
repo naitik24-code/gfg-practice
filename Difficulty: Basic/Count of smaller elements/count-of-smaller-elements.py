ede454ddbca669dada2b8134b5b7fde7121e5e94
naitik24-code <naitikpathak2005@gmail.com>
2026-03-14 22:45:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count of smaller elements/count-of-smaller-elements.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+class Solution:
+    def countOfElements(self, x, arr):
+        count=0
+        for i in arr:
+            if i <=x:
+                count+=1
+        return count
+
