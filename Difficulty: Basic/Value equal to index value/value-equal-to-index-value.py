61812a2f8b95bb3677366427f1d8672c27137568
naitik24-code <naitikpathak2005@gmail.com>
2026-03-19 16:34:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Value equal to index value/value-equal-to-index-value.py
@@ -0,0 +1,9 @@
+class Solution:
+    def valueEqualToIndex(self, arr):
+        result = []
+
+        for i in range(len(arr)):
+            if arr[i] == i + 1:
+                result.append(i + 1)
+
+        return result
