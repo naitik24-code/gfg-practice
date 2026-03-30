99df321101ec96cf4f6e0eb333f8552ed8b9eb7e
naitik24-code <naitikpathak2005@gmail.com>
2026-03-30 12:05:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Last index of One/last-index-of-one.py
@@ -0,0 +1,11 @@
+#User function Template for python3
+
+class Solution:
+    def lastIndex(self, s: str) -> int:
+        for i in range(len(s)-1,-1,-1 ):
+            if s[i]=="1":
+                return i
+        return -1
+        
+        
+        
